f4df16e62dd5debbe760d01dfa0f155deccfae87
Thao Lena Nguyen Truong <tt047@hdm-stuttgart.de>
2025-11-27 12:09:20 +0100

    DB
    Ich habe die Datenbank integriert

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,11 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import db from '@adonisjs/lucid/services/db'
 
 router.on('/').render('pages/home')
+
+router.get('/test-db', async () => {
+    const products = await db.from('products').select('*').limit(5)
+    return products
+  })
