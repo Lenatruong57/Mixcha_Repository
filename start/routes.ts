2a7bf0466c2079501c13a81c4ec5715ce6ac8bff
JeromeMaier04 <jm193@hdm-stuttgart.de>
2025-11-27 12:23:01 +0100

    Update routes.ts
    hab routen gemacht

--- start/routes.ts
+++ start/routes.ts
@@ -1,18 +1,1 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
-import db from '@adonisjs/lucid/services/db'
-
-router.on('/').render('pages/home')
-
-router.get('/test-db', async () => {
-    const products = await db.from('products').select('*').limit(5)
-    return products
-  })
