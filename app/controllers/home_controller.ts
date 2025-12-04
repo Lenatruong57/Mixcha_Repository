a5d597ce411a024e9ef6688e0bec9930fe4aed2b
florianhasani <fh127@hdm-stuttgart.de>
2025-12-04 12:51:03 +0100

    Update home_controller.ts

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,13 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HomeController {
-<<<<<<< HEAD
-    public async index({ view }: HttpContext) {
-        return view.render('pages/home')
-    }     
-=======
   public async index({ view }: HttpContext) {
-    return view.render('pages/home')   // dein Edge-Template
+    return view.render('pages/home')   
   }
->>>>>>> 98e83aa35cad394195e31b358afe6de7c59706b2
 }
