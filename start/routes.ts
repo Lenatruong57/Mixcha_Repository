dff6b632fb6bd3f7c3bf0b88ff5075a050188ec7
florianhasani <fh127@hdm-stuttgart.de>
2025-12-17 17:25:25 +0100

    agb, impressum, datenschutz grundstruktur

--- start/routes.ts
+++ start/routes.ts
@@ -31,4 +31,3 @@ router.post('/warenkorb/add', [WarenkorbsController, 'add'])
 router.post('/warenkorb/update-quantity', [WarenkorbsController, 'updateQuantity'])
 router.post('/warenkorb/remove-item', [WarenkorbsController, 'removeItem'])
 router.post('/user', [UserController, 'login'])
-
