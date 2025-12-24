d80fce8ff15414ccd268744575fad95ae57ad964
Thao Lena Nguyen Truong <tt047@hdm-stuttgart.de>
2025-12-24 03:35:52 +0100

    Header, Footer und Produktseite
    Ich habe Header und Footer erstellt, damit wir es immer einbetten können, Es an Home und AGB ausprobiert, ob alles klappt und dann mit Produktseite angefangen und bin fast fertig damit

--- app/controllers/blog_controller.ts
+++ app/controllers/blog_controller.ts
@@ -3,7 +3,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class BlogController {
   public async index({ view }: HttpContext) {
-    // Später durch DB-Abfrage ersetzen
+ 
     const posts = [
       { id: 1, title: 'Matcha Basics', teaser: 'Was ist Matcha eigentlich?' },
       { id: 2, title: 'Rezepte', teaser: 'Latte, Iced Matcha & mehr' },
@@ -17,7 +17,6 @@ export default class BlogController {
   public async show({ params, view }: HttpContext) {
     const id = Number(params.id)
 
-    // Platzhalter
     const post = {
       id,
       title: `Blog-Artikel #${id}`,
